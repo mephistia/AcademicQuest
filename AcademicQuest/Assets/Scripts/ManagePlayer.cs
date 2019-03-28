1e1271e7b3e4354fdf5f421a2541f8f92f21160a
Julia Castro Stefanello <39158254+mephistia@users.noreply.github.com>
2019-03-27 21:04:44 -0300

    First

---
+++ AcademicQuest/Assets/Scripts/ManagePlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagePlayer : MonoBehaviour
+
+{
+
+    // atributos
+    public int vida, energia, distancia, resistencia, agilidade, poder, pilas, elemento;
+    public string tipoArma, tipoArmadura, armaEquip, armaduraEquip, amuleto1, amuleto2, nome;
+
+
+    // manter entre cenas
+    void Awake()
+    {
+
+        DontDestroyOnLoad(this);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // funções
+    public void NovoJogo()
+    {
+        energia = 10;
+        pilas = 0;
+
+
+    }
+}
